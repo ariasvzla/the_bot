ed383e870ec6ec23f6f6389e08df461ec42d888e
Elian <arias.eliann@gmail.com>
2024-06-10 19:21:44 +0200

    Adding new coins

--- the_bot/constants.py
+++ the_bot/constants.py
@@ -1,11 +1,13 @@
 bot_domain = "bot.solesbot.ai"
 coins = [
-    {"abb": "ARB", "max_profit": 0.86, "max_to_invest": 300, "priority": 2},
-    {"abb": "VET", "max_profit": 0.60, "max_to_invest": 300, "priority": 6},
-    {"abb": "LDO", "max_profit": 0.65, "max_to_invest": 300, "priority": 5},
+    {"abb": "ARB", "max_profit": 0.86, "max_to_invest": 300, "priority": 5},
+    {"abb": "VET", "max_profit": 0.60, "max_to_invest": 300, "priority": 9},
+    {"abb": "LDO", "max_profit": 0.65, "max_to_invest": 300, "priority": 6},
     {"abb": "FTM", "max_profit": 1.68, "max_to_invest": 100, "priority": 0},
     {"abb": "CAKE", "max_profit": 1.88, "max_to_invest": 30, "priority": 3},
-    {"abb": "ATOM", "max_profit": 0.81, "max_to_invest": 1000, "priority": 4},
-    {"abb": "FIL", "max_profit": 0.55, "max_to_invest": 1000, "priority": 7},
+    {"abb": "ATOM", "max_profit": 0.81, "max_to_invest": 1000, "priority": 7},
+    {"abb": "FIL", "max_profit": 0.55, "max_to_invest": 1000, "priority": 8},
     {"abb": "DOT", "max_profit": 0.91, "max_to_invest": 500, "priority": 1},
+    {"abb": "AVAX", "max_profit": 0.86, "max_to_invest": 500, "priority": 2},
+    {"abb": "LINK", "max_profit": 0.86, "max_to_invest": 500, "priority": 4},
 ]
